207f56db71acd9dcda355eb4bc539ed3c0d80a86
Haybuka <paschal.nduleo@gmail.com>
2020-12-11 23:00:28 +0100

    Handled logic for user picking

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,11 +7,21 @@
     let paper = document.querySelector('.paper--img')
     let scissors = document.querySelector('.scissors--img')
     let rock = document.querySelector('.rock--img')
+    let img = document.querySelector('.game--compare img')
     let gameItem = [paper,scissors,rock];
+
    gameItem.forEach( function(item){
        item.addEventListener('click',function(){
           hideGamePick.classList.add('hidden');
           gameDecide.classList.remove('hidden')
+          console.log(this.id)
+          for(let i=0;i<gameData.length; i++){
+              if(this.id == gameData[i].name){
+                  img.setAttribute('src',gameData[i].img)
+                  img.setAttribute('class',gameData[i].class)
+                img.getAttribute('class')
+              }
+          }
       })
     
    })
@@ -29,3 +39,22 @@
       hideGamePick.classList.remove('hidden')
       gameDecide.classList.add('hidden')
     })
+    let gameData = [
+        {
+            name : "scissor",
+            img : "assets/images/icon-scissors.svg",
+            class:"scissors--img"
+           },
+           {
+            name : "paper",
+            img : "assets/images/icon-paper.svg",
+            class : "paper--img"
+           },
+           {
+            name : "rock",
+            img : "assets/images/icon-rock.svg",
+            class:"rock--img"
+           }
+    ]
+       
+    
