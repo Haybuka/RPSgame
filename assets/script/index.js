143500b55fb535220fe2da2f2d72f05ed1cf4533
Haybuka <paschal.nduleo@gmail.com>
2020-12-11 23:37:19 +0100

    Refactored javascript to be readable

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,38 +7,50 @@
     let paper = document.querySelector('.paper--img')
     let scissors = document.querySelector('.scissors--img')
     let rock = document.querySelector('.rock--img')
-    let img = document.querySelector('.game--compare img')
+    let imgUser = document.querySelector('#pickA')
+    let imgHouse = document.querySelector('#pickB')
     let gameItem = [paper,scissors,rock];
 
-   gameItem.forEach( function(item){
+//   iterating over game Item to determine choice
+    gameItem.forEach( function(item){
        item.addEventListener('click',function(){
+        //    removing and adding classes
           hideGamePick.classList.add('hidden');
           gameDecide.classList.remove('hidden')
-          console.log(this.id)
+
+        //   selecting user choice from array gameData
           for(let i=0;i<gameData.length; i++){
-              if(this.id == gameData[i].name){
-                  img.setAttribute('src',gameData[i].img)
-                  img.setAttribute('class',gameData[i].class)
-                img.getAttribute('class')
-              }
-          }
+            if(this.id == gameData[i].name){
+                imgUser.setAttribute('src',gameData[i].img)
+                imgUser.setAttribute('class',gameData[i].class)
+            }
+        }
+
+        //   House Choice using Random Number
+         let rand = randx()
+          imgHouse.setAttribute('src',gameData[`${rand}`].img)
+          imgHouse.setAttribute('class',gameData[`${rand}`].class)
       })
     
    })
-    // hideGamePick.addEventListener('click', function () {
-    //   this.classList.add('hidden');
-    //   gameDecide.classList.remove('hidden')
-    // })
+
+//Handling Close of Rules Section
     closeRules.addEventListener('click', () => {
       rules.classList.add('close');
     })
+
+// Handling Open for Rules Section 
     openRules.addEventListener('click', () => {
       rules.classList.remove('close');
     })
+
+//Play Button to Toggle Rules
     playButton.addEventListener('click', function () {
       hideGamePick.classList.remove('hidden')
       gameDecide.classList.add('hidden')
     })
+
+//Game Data to store Data of Items and enable easy Automation
     let gameData = [
         {
             name : "scissor",
@@ -56,5 +68,8 @@
             class:"rock--img"
            }
     ]
-       
-    
+
+//Function to Pick Random Number 
+    let randx = function pickHouse(){
+        return Math.floor(Math.random()*gameData.length)
+                }
